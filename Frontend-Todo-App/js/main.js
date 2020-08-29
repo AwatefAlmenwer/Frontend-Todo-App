92e21c1bea8236dc13c63a67abc3c30b7fd1a4c0
AwatefAlmenwer <52204021+AwatefAlmenwer@users.noreply.github.com>
2020-08-29 23:32:24 +0300

    Add files via upload

---
+++ Frontend-Todo-App/js/main.js
@@ -0,0 +1,16 @@
+function openNav() {
+  document.getElementById("side-menu").style.width = "200px";
+  document.getElementById("main").style.marginLeft = "200px";
+}
+
+function closeNav() {
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function myFunction() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true) {
+  }
+}
